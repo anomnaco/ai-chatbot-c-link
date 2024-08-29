014bccb540a801d60cbeee6dcb3387fd49b90e39
jsbilgi <jsbilgi@yahoo.com>
2024-08-29 12:58:06 +0530

    Included readers

--- data/reader.py
+++ data/reader.py
@@ -1,4 +1,6 @@
 import yaml
+from llama_index.readers.web import BeautifulSoupWebReader
+from llama_index.readers.web import SimpleWebPageReader
 
 # Load the YAML content from an external file
 with open('urls.yml', 'r') as file:
@@ -16,5 +18,13 @@ for category, urls in data['documents'].items():
         detected_category = detect_url_category(url, data)
         if detected_category:
             print(f"The URL '{url}' belongs to the category '{detected_category}'.")
+            if detected_category in ("BeautifulSoupWebReader", "SimpleWebPageReader"):
+                if detected_category == "BeautifulSoupWebReader":
+                    loader = BeautifulSoupWebReader()
+                elif detected_category == "SimpleWebPageReader":
+                    loader = SimpleWebPageReader()
+                
+                document = loader.load_data(urls=[url])
+                print(document)
         else:
             print(f"The URL '{url}' does not belong to any known category.")
