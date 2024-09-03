4d24060257eb17138b4bc4d54ede5d83bd2183be
jsbilgi <jsbilgi@yahoo.com>
2024-09-04 00:45:31 +0530

    Fetching urls from astra db in place of yaml

--- data/reader.py
+++ data/reader.py
@@ -3,16 +3,28 @@ from dotenv import load_dotenv
 import os
 import sys
 
-from llama_index.readers.web import BeautifulSoupWebReader
-from llama_index.readers.web import SimpleWebPageReader
-from llama_index.readers.web import AsyncWebPageReader
-from llama_index.readers.web import BrowserbaseWebReader
-from llama_index.readers.web import MainContentExtractorReader
-from llama_index.readers.web import NewsArticleReader
-from llama_index.readers.web import ReadabilityWebPageReader
-from llama_index.readers.web import RssReader
-from llama_index.readers.web import TrafilaturaWebReader
-from llama_index.readers.web import UnstructuredURLLoader
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+from llama_index.readers.web import (
+    AsyncWebPageReader,
+    BeautifulSoupWebReader,
+    BrowserbaseWebReader,
+    FireCrawlWebReader,
+    KnowledgeBaseWebReader,
+    MainContentExtractorReader,
+    NewsArticleReader,
+    ReadabilityWebPageReader,
+    RssNewsReader,
+    RssReader,
+    ScrapflyReader,
+    SimpleWebPageReader,
+    SitemapReader,
+    SpiderWebReader,
+    TrafilaturaWebReader,
+    UnstructuredURLLoader,
+    WholeSiteReader
+)
 import nltk
 
 nltk.download('punkt')
@@ -20,30 +32,196 @@ nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('averaged_perceptron_tagger_eng')
 
-#Failed
-#from llama_index.readers.web.firecrawl_web.base import FireCrawlWebReader
-#from llama_index.readers.web import KnowledgeBaseWebReader
-#from llama_index.core.readers.web.rss_news import RSSNewsReader # ModuleNotFoundError: No module named 'llama_index.core.readers.web'
-# from llama_index.readers.web import SitemapReader 
-# from llama_index.readers.web import WholeSiteReader # chromedriver version mismatch
-
-#TODO Readers
-# from llama_index.readers.web import ScrapflyReader #Pending due to api key waiting for approval
-# from llama_index.readers.web import SpiderWebReader #Pending due to api key need to pay to get api key
-
 load_dotenv()
+url_output_directory = "output"
 browserbase_api_key = os.getenv("BROWSERBASE_API_KEY")
 firecrawl_api_key = os.getenv("FIRECRAWL_API_KEY")
+scrapfly_api_key = os.getenv("SCRAPFLY_API_KEY")
 
-#Creat folder if not exists
+# Astra DB credentials
+client_id = os.getenv("ASTRA_DB_CLIENT_ID")
+client_secret = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+keyspace = os.getenv("ASTRA_DB_KEY_SPACE")
+secure_connect_bundle = os.getenv("ASTRA_DB_SECURE_CONNECT_BUNDLE")
+config_table_name = os.getenv("ASTRA_DB_CONFIG_TABLE_NAME")
+if not keyspace:
+    keyspace = "default_keyspace"
+
+# Astra DB connection
+cloud_config = {
+    'secure_connect_bundle': secure_connect_bundle
+}
+auth_provider = PlainTextAuthProvider(client_id, client_secret)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect(keyspace)
+
+# Create folder if not exists
 def create_folder_if_not_exists(folder_path):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         print(f"Folder '{folder_path}' created.")
 
-# Usage:
-url_output_directory="output"
 create_folder_if_not_exists(url_output_directory)
+
+# Base class for all readers
+class BaseReader:
+    def load(self, url):
+        raise NotImplementedError("Each reader must implement the `load` method.")
+
+# Individual reader classes
+class AsyncWebPageReaderHandler(BaseReader):
+    def load(self, url):
+        return AsyncWebPageReader().load_data(urls=[url])
+
+class BeautifulSoupWebReaderHandler(BaseReader):
+    def load(self, url):
+        return BeautifulSoupWebReader().load_data(urls=[url])
+
+class BrowserbaseWebReaderHandler(BaseReader):
+    def __init__(self):
+        self.api_key = os.getenv("BROWSERBASE_API_KEY")
+        if not self.api_key:
+            raise ValueError("API key is required for BrowserbaseWebReader")
+
+    def load(self, url):
+        return BrowserbaseWebReader(api_key=self.api_key).load_data(urls=[url])
+
+class FireCrawlWebReaderHandler(BaseReader):
+    def __init__(self):
+        self.api_key = os.getenv("FIRECRAWL_API_KEY")
+        if not self.api_key:
+            raise ValueError("API key is required for FireCrawlWebReader")
+
+    def load(self, url):
+        return FireCrawlWebReader(api_key=self.api_key).load_data(urls=[url])
+
+class KnowledgeBaseWebReaderHandler(BaseReader):
+    def load(self, url):
+        return KnowledgeBaseWebReader(root_url=url,link_selectors=[".mw-parser-output"], article_path="content").load_data()
+
+class MainContentExtractorReaderHandler(BaseReader):
+    def load(self, url):
+        return MainContentExtractorReader().load_data(urls=[url])
+
+class NewsArticleReaderHandler(BaseReader):
+    def load(self, url):
+        return NewsArticleReader(use_nlp=False).load_data(urls=[url])
+
+class ReadabilityWebPageReaderHandler(BaseReader):
+    def load(self, url):
+        return ReadabilityWebPageReader().load_data(url = url)
+
+class RSSNewsReaderHandler(BaseReader):
+    def load(self, url):
+        rssNewsReader = download_loader("RSSNewsReader")
+        return rssNewsReader().load_data(urls=[url])
+
+class RSSReaderHandler(BaseReader):
+    def load(self, url):
+        return RssReader().load_data(urls=[url])
+
+class ScrapflyReaderHandler(BaseReader):
+    def __init__(self):
+        self.api_key = os.getenv("SCRAPFLY_API_KEY")
+        if not self.api_key:
+            raise ValueError("API key is required for ScrapflyWebReader")
+
+    def load(self, url):
+        scrapfly_reader = ScrapflyReader(api_key=self.api_key, ignore_scrape_failures=True)
+        scrapfly_scrape_config = {
+            "asp": True,  # Bypass scraping blocking and antibot solutions, like Cloudflare
+            "render_js": True,  # Enable JavaScript rendering with a cloud headless browser
+            "proxy_pool": "public_residential_pool",  # Select a proxy pool (datacenter or residnetial)
+            "country": "us",  # Select a proxy location
+            "auto_scroll": True,  # Auto scroll the page
+            "js": "",  # Execute custom JavaScript code by the headless browser
+            }
+        return scrapfly_reader.load_data(urls=[url], scrape_config=scrapfly_scrape_config, scrape_format="markdown")
+
+class SimpleWebPageReaderHandler(BaseReader):
+    def load(self, url):
+        return SimpleWebPageReader().load_data(urls=[url])
+
+class SitemapReaderHandler(BaseReader):
+    def load(self, url):
+        return SitemapReader().load_data(sitemap_url=url)
+
+class SpiderWebReaderHandler(BaseReader):
+    def __init__(self):
+        self.api_key = os.getenv("SPIDERWEB_API_KEY")
+        if not self.api_key:
+            raise ValueError("API key is required for ScrapflyWebReader")
+
+    def load(self, url):
+        return SpiderWebReader().load_data(url=url)
+
+class TrafilaturaWebReaderHandler(BaseReader):
+    def load(self, url):
+        return TrafilaturaWebReader().load_data(urls=[url])
+
+class UnstructuredURLLoaderHandler(BaseReader):
+    def load(self, url):
+        return UnstructuredURLLoader(urls=[url], continue_on_failure=False, headers={"User-Agent": "value"}).load_data()
+
+class WholeSiteReaderHandler(BaseReader):
+    def load(self, url):
+        try:
+            import chromedriver_autoinstaller
+        except ImportError:
+            raise ImportError("Please install chromedriver_autoinstaller")
+        from llama_index.readers.web import WholeSiteReader
+        from selenium import webdriver
+
+        options = webdriver.ChromeOptions()
+        options.binary_location = "/usr/bin/google-chrome"
+        options.add_argument("--start-maximized")
+        options.add_argument("--headless")
+        chromedriver_autoinstaller.install()
+        driver = webdriver.Chrome(options=options)
+
+        # Initialize the scraper with a prefix URL and maximum depth
+        scraper = WholeSiteReader(
+            prefix="https://www.paulgraham.com/",
+            max_depth=10,  # Example prefix
+            driver=driver,  # Your custom driver with correct options
+        )
+
+        return scraper.load_data(base_url=url)
+
+# Factory function to create readers
+def reader_factory(category):
+    reader_classes = {
+        "AsyncWebPageReader": AsyncWebPageReaderHandler,
+        "BeautifulSoupWebReader": BeautifulSoupWebReaderHandler,
+        "BrowserbaseWebReader": BrowserbaseWebReaderHandler,
+        "FireCrawlWebReader": FireCrawlWebReaderHandler,
+        "KnowledgeBaseWebReader": KnowledgeBaseWebReaderHandler,
+        "MainContentExtractorReader": MainContentExtractorReaderHandler,
+        "NewsArticleReader": NewsArticleReaderHandler,
+        "ReadabilityWebPageReader":ReadabilityWebPageReaderHandler,
+        "RssNewsReader": RSSNewsReaderHandler,
+        "RssReader":RSSReaderHandler,
+        "ScrapflyReader": ScrapflyReaderHandler,
+        "SimpleWebPageReader": SimpleWebPageReaderHandler,
+        "SitemapReader": SitemapReaderHandler,
+        "SpiderWebReader": SpiderWebReaderHandler,
+        "TrafilaturaWebReader": TrafilaturaWebReaderHandler,
+        "UnstructuredURLLoader": UnstructuredURLLoaderHandler,
+        "WholeSiteReader": WholeSiteReaderHandler
+    }
+    reader_class = reader_classes.get(category)
+    if not reader_class:
+        raise ValueError(f"No reader found for category {category}")
+    return reader_class()
+
+# Function to sanitize file names
+def sanitize_filename(url):
+    file_name = url.replace("http://www.","").replace("https://www.","").replace("http://","").replace("https://","").replace("/","-")
+    question_mark_position = file_name.find("?")
+    file_name = file_name[:question_mark_position] if question_mark_position != -1 else file_name
+    if file_name.endswith("-"):
+        file_name = file_name[:-1]
+    return file_name
 
 # Load the YAML content from an external file
 with open('urls.yml', 'r') as file:
@@ -61,99 +239,20 @@ try:
 except:
     pass
 
-# Traverse through all URLs and print the category for each
-for category, urls in data['documents'].items():
-    for url in urls:
-        document = None
-        reader=0
-        detected_category = detect_url_category(url, data)
-        if detected_category:
-            try:
-                if not reader_name or detected_category in (reader_name):
-                    if detected_category == "BeautifulSoupWebReader":
-                        loader = BeautifulSoupWebReader()
-                        reader=1
-                    elif detected_category == "SimpleWebPageReader":
-                        loader = SimpleWebPageReader()
-                        reader=1
-                    elif detected_category == "AsyncWebPageReader":
-                        loader = AsyncWebPageReader()
-                        reader=1
-                    elif detected_category == "BrowserbaseWebReader":
-                        loader = BrowserbaseWebReader(browserbase_api_key, None)
-                        reader=1
-                    # elif detected_category == "FireCrawlWebReader":
-                    #    loader = FireCrawlWebReader(api_key=firecrawl_api_key, mode="crawl")
-                    #    document = loader.load_data(url=url) # firecrawl_web\base.py  line 93 text=doc.get("markdown", "") fails with AttributeError: 'str' object has no attribute 'get'
-                    #elif detected_category == "KnowledgeBaseWebReader":
-                    #     loader = KnowledgeBaseWebReader(root_url=url, link_selectors=[".mw-parser-output"], article_path="content")
-                    #     document = loader.load_data() # knowledge_base\base.py", line 160 fails with TypeError: can only concatenate str (not "NoneType") to str
-                    elif detected_category == "MainContentExtractorReader":
-                        loader = MainContentExtractorReader()
-                        reader=1
-                    elif detected_category == "NewsArticleReader":
-                        loader = NewsArticleReader(use_nlp=False)
-                        reader=1
-                    #elif detected_category == "ReadabilityWebPageReader":
-                    #    loader = ReadabilityWebPageReader()
-                    #    document = loader.load_data(url=url)
-                    #elif detected_category == "RssReader":
-                    #    loader = RssReader()
-                    #    rss_feed_url = url
-                    #    document = loader.load_data(urls=[rss_feed_url]) # Failed with object has no attribute 'description'
-                    #elif detected_category == "RSSNewsReader":
-                    #    RSSNewsReader = download_loader("RSSNewsReader")
-                    #    loader = RSSNewsReader()
-                    #elif detected_category == "ScrapflyReader":
-                    #   # Initiate ScrapflyReader with your ScrapFly API key
-                    #     scrapfly_reader = ScrapflyReader(
-                    #         api_key="Your ScrapFly API key",  # Get your API key from https://www.scrapfly.io/
-                    #         ignore_scrape_failures=True,  # Ignore unprocessable web pages and log their exceptions
-                    #     )
-                    #     scrapfly_scrape_config = {
-                    #         "asp": True,  # Bypass scraping blocking and antibot solutions, like Cloudflare
-                    #         "render_js": True,  # Enable JavaScript rendering with a cloud headless browser
-                    #         "proxy_pool": "public_residential_pool",  # Select a proxy pool (datacenter or residnetial)
-                    #         "country": "us",  # Select a proxy location
-                    #         "auto_scroll": True,  # Auto scroll the page
-                    #         "js": "",  # Execute custom JavaScript code by the headless browser
-                    #     }
-                    #     # Load documents from URLs as markdown
-                    #     document = scrapfly_reader.load_data(
-                    #         urls=[url],
-                    #         scrape_config=scrapfly_scrape_config,  # Pass the scrape config
-                    #         scrape_format="markdown",  # The scrape result format, either `markdown`(default) or `text`
-                    #     )
-                    #elif detected_category == "SitemapReader":
-                    #     loader = SitemapReader()
-                    #     document = loader.load_data(sitemap_url=url)
-                    #elif detected_category == "SpiderWebReader":
-                    #     loader = SpiderWebReader(api_key="YOUR_API_KEY",  mode="scrape")
-                    #     document = loader.load_data(url=url)
-                    elif detected_category == "TrafilaturaWebReader":
-                        loader = TrafilaturaWebReader()
-                        reader=1
-                    elif detected_category == "UnstructuredURLLoader":
-                        loader = UnstructuredURLLoader(urls=[url], continue_on_failure=False, headers={"User-Agent": "value"})
-                        document = loader.load_data()
-                    #elif detected_category == "WholeSiteReader":
-                    #     loader = WholeSiteReader(prefix="https://www.paulgraham.com/", max_depth=10)
-                    #     document = loader.load_data(base_url=url)
+query = "SELECT url, category FROM " + config_table_name + ";"
+rows = session.execute(query)
 
-                    if reader==1:
-                        document = loader.load_data(urls=[url])
+# Main loop to process URLs
+for url , category in rows:
+    if not reader_name or category in (reader_name):
+        try:
+            reader = reader_factory(category)
+            document = reader.load(url)
+            if document:
+                file_name = sanitize_filename(url)
+                with open(os.path.join(url_output_directory, category+"_"+file_name), "w", encoding='utf-8') as f:
+                    f.write(document[0].text)
+        except Exception as e:
+            with open("url_error.txt", "a", encoding='utf-8') as f:
+                f.write(f"Category: {category} url: {url} {str(e)}\n")
 
-                    file_name = url.replace("http://www.","").replace("https://www.","").replace("http://","").replace("https://","").replace("/","-")
-                    question_mark_position = file_name.find("?")
-                    file_name = file_name[:question_mark_position]
-                    if file_name[-1:] == "-":
-                        file_name=file_name[:-1]
-
-                    if document:
-                        with open(url_output_directory+"/"+ file_name, "w", encoding='utf-8') as f:
-                            f.write(document[0].text)
-            except Exception as e:
-                with open("url_error.txt", "a", encoding='utf-8') as f:
-                    f.write("Category :" + detected_category + " url : " + url + " " + str(e) + "\n")
-        else:
-            print(f"The URL '{url}' does not belong to any known category.")
